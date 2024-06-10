3cf9ea054e5f87e2769ae779d378808cace5f89a
felodenz <46296993+felodenz@users.noreply.github.com>
2024-06-10 17:22:34 +0330

    Update handler.js
    some ES6  👍

--- handler/handler.js
+++ handler/handler.js
@@ -1,7 +1,7 @@
-import express from "express";
+import {Router} from "express";
 import paramsHandle from "../controllers/paramsHandler.js";
 import queryHandle from "../controllers/queryHandler.js";
-const router = express.Router();
+const router = Router();
 router.use(express.json());
 
 router.post("/trainingv1.0", queryHandle);
